f4c27828918c4b4f35fba4c0a1d24dad6641946e
Caique L Castro <caique.castro@morlan.com.br>
2026-04-07 12:48:16 -0300

    changing first page

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
-import TaskPage from './pages/TaskPages.jsx';
+import TaskPage from "./pages/TaskPages.jsx";
 
 const router = createBrowserRouter([
   {
-    path: "/home",
+    path: "/",
     element: <App />,
   },
 
   {
     path: "/task",
     element: <TaskPage />,
-  }
+  },
 ]);
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />,
   </StrictMode>,
-  
-)
+);
